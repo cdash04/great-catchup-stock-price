c68e41bcf1c08d5caf1ef173db2b7896483383ff
Christophe Duchesne-Ashworth <christophe.ashworth@zendesk.com>
2023-03-21 17:47:24 -0400

    fresh start without learna (it really suck)

--- packages/price-charting-client/lib/client/base-client.ts
+++
@@ -1,9 +0,0 @@
-import { AxiosRequestConfig } from 'axios';
-
-const baseClientCongif: AxiosRequestConfig = {
-  responseType: 'json',
-  headers: {
-    'Content-Type': 'application/json',
-    'Cache-Control': 'no-cache',
-  },
-};
